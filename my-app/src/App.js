5c69f6ea0ca9129f104d03c8535f24aefd2dfeed
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-07-18 12:25:34 -0700

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import Dictionary from "./dictionary";
+import "./App.css";
+import logo from "./dic.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
