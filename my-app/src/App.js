f2d6b9505691ba57ceaa0fc0c73874a857412488
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-07-19 10:03:23 -0700

    add link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,9 +13,14 @@ function App() {
         <main>
           <Dictionary />
         </main>
-     
+
         <footer className="App-footer">
-          <small>Coded by ME</small>
+          <small>
+            Coded by ME on{" "}
+            <a href="https://github.com/CookieCutter13/dictionary1/tree/main/my-app/src">
+              Github
+            </a>{" "}
+          </small>
         </footer>
       </div>
     </div>
